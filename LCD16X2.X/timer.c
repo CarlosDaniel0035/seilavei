384467d3be0e214c0e783b6c4ec209f6839b3dd1
20185070 <20185070@DSN1271158630.cfp127aluno.local>
2021-06-17 15:27:51 -0300

    Projeto terminado3.

--- LCD16X2.X/timer.c
+++ LCD16X2.X/timer.c
@@ -17,41 +17,41 @@ unsigned int contador2;
 
 void timer2_init (void)
 {
-    INTCONbits.GIE = 0;
+    INTCONbits.GIE = 0; // desliga todos para poder mexer nos bits de interrupcão 
     T2CONbits.T2CKPS = 0; //prescaler
     T2CONbits.TOUTPS = 0; //podscaler
-    TMR2 = 0;
-    PR2 = 100;
-    contador = 0;
+    TMR2 = 0;  //comparador 
+    PR2 = 100; //comparador 
+    contador = 0; 
     t2seg = 1;
     contador2 = 0;
     T2CONbits.TMR2ON = 1; // Liga o timer2
     INTCONbits.PEIE = 1; // liga perifericos 
     PIE1bits.TMR2IE = 1; // interrompe o PR2
-    INTCONbits.GIE = 1; // liga todos os bits          
+    INTCONbits.GIE = 1; // liga todos os bits de interrupção          
 }
 
 
 void __interrupt() ossada (void)
 {
    
-    INTCONbits.GIE = 0;
-    if (PIR1bits.TMR2IF)
+    INTCONbits.GIE = 0; // desabilita todos para poder usar os bits
+    if (PIR1bits.TMR2IF) //se o comparador for ligado ele acrescenta t2seg  
     {
-        PIR1bits.TMR2IF=0;
+        PIR1bits.TMR2IF=0; // desliga comparador
         t2seg++;
-    if(!t2seg)
+    if(!t2seg) // se t2seg for chamado contador acrescenta 
     {
         contador++;
         t2seg = 10000;
     }
         {
-            if(contador ==11)
+            if(contador ==11) // se o contador chegar em 11 conta 1 e por ai vai
             {
                 contador2++;
                 contador = 0; 
             }    
             }
    } 
-    INTCONbits.GIE= 1;
+    INTCONbits.GIE= 1; // liga os bits de interrupção 
 }
