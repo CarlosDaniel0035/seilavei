e34ffd48678ebef0827392c476a1f9bd12a8204a
20185070 <20185070@DSN1271158630.cfp127aluno.local>
2021-06-17 14:45:53 -0300

    Projeto terminado.

--- LCD16X2.X/timer.c
+++ LCD16X2.X/timer.c
@@ -9,34 +9,41 @@
 #include <xc.h>
 #include "config.h"
 #include "dispLCD4vias.h"
+#include "delay.h"
 
 unsigned int contador;
 unsigned int t2seg;
 
+
 void timer2_init (void)
 {
     INTCONbits.GIE = 0;
-    T2CONbits.T2CKPS = 00;
-    T2CONbits.TOUTPS = 0000;
-    TMR2 = 256-156;
+    T2CONbits.T2CKPS = 0; //prescaler
+    T2CONbits.TOUTPS = 0; //podscaler
+    TMR2 = 0;
+    PR2 = 100;
     contador = 0;
-    t2seg = 10000;
-    INTCONbits.PEIE = 1;
-    INTCONbits.GIE = 1;           
+    t2seg = 1;
+    T2CONbits.TMR2ON = 1; // Liga o timer2
+    INTCONbits.PEIE = 1; // liga perifericos 
+    PIE1bits.TMR2IE = 1; // interrompe o PR2
+    INTCONbits.GIE = 1; // liga todos os bits          
 }
-void __interrupt() ossada(void)
+
+
+void __interrupt() ossada (void)
 {
+   
     INTCONbits.GIE = 0;
-    if(INTCONbits.PEIE)
+    if (PIR1bits.TMR2IF)
     {
-        INTCONbits.PEIE = 0;
-        TMR2 = 256-156;
-        --t2seg;
-        if ( !t2seg)
-        {
-            t2seg = 10000;
-            ++contador;
-        } 
+        PIR1bits.TMR2IF=0;
+        t2seg++;
+    if(!t2seg)
+    {
+        contador++;
+        t2seg = 10000;
     }
-    INTCONbits.GIE = 1;
+   } 
+    INTCONbits.GIE= 1;
 }
